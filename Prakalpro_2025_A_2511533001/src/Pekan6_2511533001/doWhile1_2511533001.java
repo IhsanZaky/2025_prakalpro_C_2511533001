852757216209381239fe4b27cf31a294845581eb
IhsanZaky <fibiscoming897@gmail.com>
2025-11-04 11:52:35 +0700

    Add files via upload

---
+++ Prakalpro_2025_A_2511533001/src/Pekan6_2511533001/doWhile1_2511533001.java
@@ -0,0 +1,16 @@
+package Pekan6_2511533001;
+
+import java.util.Scanner;
+
+public class doWhile1_2511533001 {
+
+	public static void main(String[] args) {
+	Scanner console = new Scanner(System.in);
+	String phrase;
+	do {
+		System.out.print("Input Password: ");
+		phrase = console.next();
+		} while (!phrase.equals("abcd"));
+	console.close();
+	}
+}
