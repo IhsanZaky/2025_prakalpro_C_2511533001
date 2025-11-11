dd0eea10dffb3c6423208181e74b27d6f2623bc1
IhsanZaky <fibiscoming897@gmail.com>
2025-11-11 11:51:46 +0700

    Praktikum pekan 7

---
+++ Prakalpro_2025_A_2511533001/src/Pekan7_2511533001/PanggilMahasiswa_2511533001.java
@@ -0,0 +1,13 @@
+package Pekan7_2511533001;
+
+public class PanggilMahasiswa_2511533001 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533001 a= new Mahasiswa_2511533001 ();
+		a.setNim(251153);
+		a.setNama("Zaky");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
